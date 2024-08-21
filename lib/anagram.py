86b256e39c7f94c821223c598cff5ac48e69bbdf
adendaiy <adenabdib@gmail.com>
2024-08-22 00:13:35 +0100

    this is my anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        matches = []
+        sorted_word = sorted(self.word)
+        
+        for possible_anagram in word_list:
+            sorted_possible_anagram = sorted(possible_anagram)
+            
+            if sorted_word == sorted_possible_anagram:
+                matches.append(possible_anagram)
+        return matches
